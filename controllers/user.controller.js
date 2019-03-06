e6da248c14bf9a1b483c681e4c9c96fce1763bfd
Tran Dang Lam <trandanglam.exs@gmail.com>
2019-03-06 10:17:46 +0700

    postCreate

---
+++ controllers/user.controller.js
@@ -0,0 +1,38 @@
+var db=require('../db');
+var shortid = require('shortid');
+
+module.exports.index=function (req, res) {
+  res.render('users/index', {
+    users: db.get('users').value()
+  });
+};
+module.exports.search=function (req, res) {
+  var q = req.query.q;
+  var matchedUsers = db.get('users').value().filter(function (user) {
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  res.render('users/index', {
+    users: matchedUsers
+  });
+};
+
+module.exports.getCreate=function (req, res) {
+  res.render('users/create');
+};
+
+module.exports.postCreate=function (req, res) {
+  // console.log(req.body);
+  req.body.id=shortid.generate();
+  db.get('users').push(req.body).write();
+  res.redirect('/users');
+};
+
+module.exports.id=function (req, res) {
+  var id = parseInt(req.params.id);
+  var user = db.get('users').find({
+    id: id
+  }).value();
+  res.render('users/view', {
+    user: user
+  });
+};
